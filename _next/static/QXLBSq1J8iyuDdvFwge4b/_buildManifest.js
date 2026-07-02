a04a66b9c003c0befcca703133229d7b1aa6a4e5
Jon Fernández <j.fernandezde@opendeusto.es>
2026-07-02 17:07:38 +0200

    Updates

--- _next/static/QXLBSq1J8iyuDdvFwge4b/_buildManifest.js
+++ _next/static/QXLBSq1J8iyuDdvFwge4b/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mapainteractivo//_next/:path+",
+        "destination": "/mapainteractivo/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
